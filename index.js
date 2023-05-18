b0011d7630488943420b294e4e9e00eb48c235c6
Nancy D. Arena <ndarena11@gmail.com>
2023-05-18 17:44:33 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+//
+const random = Math.floor(Math.random() * 0) + 1;
+//
+const num3 = 100 ;
+const num4 = 6;
+const mod = num3 % num4;
+//
+const max = Math.max(20);
 
-// Write your code here
